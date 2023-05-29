bb17e91db1c0bd5c5796d3c892899ad5e6327630
Vardan <vkadasiev@gmail.com>
2023-05-29 12:12:26 +0400

    task completed

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -3,7 +3,79 @@ package school.mjc.stage0.loops.task1;
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
+    public void repeatIsBad() {
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int four = 4;
+        int five = 5;
+        int six = 6;
+        int seven = 7;
+        int eight = 8;
+        int nine = 9;
+        int ten = 10;
+        int eleven = 11;
+        int twelve = 12;
+        int thirteen = 13;
+        int fourteen = 14;
+        int fifteen = 15;
+        int sixteen = 16;
+        int seventeen = 17;
+        int eighteen = 18;
+        int nineteen = 19;
+        int twenty = 20;
+
+        if (one == 1) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (two == 2) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (three == 3) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (four == 4) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (five == 5) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (six == 6) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (seven == 7) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (eight == 8) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (nine == 9) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (ten == 10) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (eleven == 11) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (twelve == 12) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (thirteen == 13) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (fourteen == 14) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (fifteen == 15) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (sixteen == 16) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (seventeen == 17) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (eighteen == 18) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (nineteen == 19) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        } if (twenty == 20) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }
 
     }
-}
+        public static void main(String[] Args) {
+            RepeatIsBad printer = new RepeatIsBad();
+            printer.repeatIsBad();
+        }
+
+    }
+
+
+
+
+//writing the same code doesn't have much impact, and it's also time consuming
